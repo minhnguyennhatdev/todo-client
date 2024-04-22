67f41ced96094bcaa6921542eac80571d0eefa98
Minh Nguyen <nhatminhnguyen061200@gmail.com>
2024-04-22 13:47:10 +0700

    todo app

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+echo "Feching origin..."
+
+git fetch --all && git reset --hard @{u}
+
+echo "Deploying..."
+
+yarn
+
+yarn build
+
+pm2 restart ecosystem.config.js
+
+# SERVICE='todo-server'
+# PORT=8000
+
+# docker stop $SERVICE
+# docker rm $SERVICE
+# docker build --tag $SERVICE .
+# docker run --name $SERVICE -d -p $PORT:$PORT $SERVICE
+
+echo "Done."
